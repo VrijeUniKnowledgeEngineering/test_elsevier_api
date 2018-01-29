9e7b3fc9fa8081137a155448de79058394ea4ee0
potason <finn.potason@gmail.com>
2018-01-29 19:05:08 +0100

    more info

--- exampleProg.py
+++ exampleProg.py
@@ -18,17 +18,25 @@ client = ElsClient(config['apikey'])
 ## Author example
 # Initialize author with uri
 my_auth = ElsAuthor(
-        uri = 'https://api.elsevier.com/content/author/author_id/7004367821')
+        uri = 'https://api.elsevier.com/content/author/author_id/56923985200')
 # Read author data, then write to disk
 if my_auth.read(client):
     print ("my_auth.full_name: ", my_auth.full_name)
-    my_auth.write()
+    print ("my_auth.last_name:", my_auth.last_name)
+    print ("my_auth.id: ", my_auth.id)
+    print ("my_auth.uri: ", my_auth.uri)
+
+    my_auth.read_docs(client)
+
+    # print ("my_auth keys: ", my_auth.data.keys())
+    # my_auth.write()
+    # print ("history: ", my_auth.data['affiliation-history'])
 else:
     print ("Read author failed.")
 
 ## Affiliation example
 # Initialize affiliation with ID as string
-my_aff = ElsAffil(affil_id = '60101411')
+my_aff = ElsAffil(affil_id = '60008734')
 if my_aff.read(client):
     print ("my_aff.name: ", my_aff.name)
     my_aff.write()
@@ -37,7 +45,7 @@ else:
 
 ## Scopus (Abtract) document example
 # Initialize document with ID as integer
-scp_doc = AbsDoc(scp_id = 84872135457)
+scp_doc = AbsDoc(scp_id = 56923985200)
 if scp_doc.read(client):
     print ("scp_doc.title: ", scp_doc.title)
     scp_doc.write()
